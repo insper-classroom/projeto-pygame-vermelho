b6c25ddf510d7c2bb869a4fc35332a50937ce595
RicardolCarvalho <ricardolc2@al.insper.edu.br>
2023-10-24 10:31:50 -0300

    Constantes

--- jogo.py
+++ jogo.py
@@ -1,6 +1,20 @@
 import pygame
+from constantes import *
 
 def inicializa():
     pygame.init()
     tela = pygame.display.set_mode((800, 600), 0, 0)
     pygame.display.set_caption('Jogo')
+
+    return tela
+
+def eventos():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+
+    return True
+
+def atualiza(tela):
+    tela.fill((0, 0, 0))
+    pygame.display.update()
