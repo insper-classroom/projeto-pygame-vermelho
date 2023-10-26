23218e82eff613adcacfda92823e0dab9829c456
RicardolCarvalho <ricardolc2@al.insper.edu.br>
2023-10-26 13:53:02 -0300

    Movimentação do jogador

--- classes.py
+++ classes.py
@@ -9,6 +9,7 @@ class Jogo:
         self.game = True
         self.tela = pygame.display.set_mode((1200, 600), 0, 0)
         pygame.display.set_caption('Jogo')
+        self.relogio = pygame.time.Clock()
 
     def eventos(self):
         mapa = Mapa()
@@ -17,8 +18,25 @@ class Jogo:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.game = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        jogador.set_velocidade_x(-1)
+                    elif event.key == pygame.K_RIGHT:
+                        jogador.set_velocidade_x(1)
+                    elif event.key == pygame.K_UP:
+                        jogador.set_velocidade_y(-3)
+                    elif event.key == pygame.K_DOWN:
+                        jogador.set_velocidade_y(3)
+                if event.type == pygame.KEYUP:
+                    jogador.set_velocidade_x(0)
+                    jogador.set_velocidade_y(0)
+
+                   
+            jogador.mover()
+            jogador.atualizar_animacao()
             mapa.desenha(self.tela)
             jogador.desenha(self.tela)
+            self.relogio.tick(60)
             pygame.display.update()
         return True
 jogo = Jogo()
