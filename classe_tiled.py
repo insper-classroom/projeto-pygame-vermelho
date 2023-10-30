b54656427dcc560d58b465dff58e50f05cfd2364
RicardolCarvalho <ricardolc2@al.insper.edu.br>
2023-10-30 08:04:38 -0300

    Colisões

--- classe_tiled.py
+++ classe_tiled.py
@@ -1,8 +1,9 @@
 import pygame
+from classe_jogador import *
 from pytmx.util_pygame import load_pygame
 
 class TiledMap(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups):  
         super().__init__(groups)
         self.tmx_data = None
         self.pos = pos
@@ -11,8 +12,14 @@ class TiledMap(pygame.sprite.Sprite):
         self.tmx_data = load_pygame(arquivo)
 
     def desenhar_mapa(self, tela):
+        tiles = []
         for layer in self.tmx_data.visible_layers:
             if hasattr(layer, 'tiles'):
                 for x, y, surf in layer.tiles():
                     pos = (x * 16, y * 16)
+                    surf = pygame.transform.scale(surf, (16, 16))
                     tela.blit(surf, pos)
+                    pygame.draw.rect(tela, (255, 255, 255), (pos[0], pos[1], 16, 16), 1)
+                    #adicionar o tile a lista de tiles
+                    tiles.append(pygame.Rect(pos[0], pos[1], 16, 16))
+        return tiles
